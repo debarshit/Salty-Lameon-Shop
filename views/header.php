3d1cc9e9176dd16ff84d921b6362352e8fac6b3f
debarshi <dune02deb@gmail.com>
2026-03-11 17:53:59 +0530

    gtm added

--- views/header.php
+++ views/header.php
@@ -29,13 +29,12 @@ ob_start();
     new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
     j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
     'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-    })(window,document,'script','dataLayer','GTM-PMC7KDVW');</script>
+    })(window,document,'script','dataLayer','GTM-KRLQ9BBC');</script>
     <!-- End Google Tag Manager -->
-
   </head>
   <body>
     <!-- Google Tag Manager (noscript) -->
-    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PMC7KDVW"
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KRLQ9BBC"
     height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
     <!-- End Google Tag Manager (noscript) -->
     <!--=============== HEADER ===============-->
